267747f4847678c1478825de528fa352256cffb0
JJPPH <JCnSH4cK4WLLaaa@gmail.com>
2022-11-16 18:54:50 +0900

    22년11월16일

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
-s = [1, 2]
-s.reverse()
-print(s)
+result_1 = 1.23
+print("%.1f" % (result_1))  # 1.2
 
+result_2 = 1.25
+print("%.1f" % (result_2))  # 1.2
 
-s = [1]
-s.reverse()
-print(s)
+result_3 = 1.251
+print("%.1f" % (result_3))  # 1.3
 
-s = []
-s.reverse()
-print(s)
+result_4 = 1.26
+print("%.1f" % (result_4))  # 1.3
