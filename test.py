36c51be9f1fccbe8bbbc74e42668a2219f423312
JJPPH <JCnSH4cK4WLLaaa@gmail.com>
2022-11-30 22:59:28 +0900

    22년 11월 30일

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+dict_example = {"f": 5, "a": 1, "c": 3, "d": 2, "b": 4}
 
 
+print(type(dict_example.items()))  # <class 'dict_items'>
+print(dict_example.items())  # dict_items([('f', 5), ('a', 1), ('c', 3), ('d', 2), ('b', 4)])
 
