0579c5a7a9d74a26d4590edb179c290d63c6029b
JJPPH <JCnSH4cK4WLLaaa@gmail.com>
2022-12-04 18:48:21 +0900

    22년 12월 3일

--- python/0~2499/1541-잃어버린 괄호.py
+++ python/0~2499/1541-잃어버린 괄호.py
@@ -1,2 +1,18 @@
 import sys
-input=sys.stdin.readline
+
+input = sys.stdin.readline
+
+string = input().rstrip()
+string_split_minus = string.split("-")
+
+result = 0
+for i in range(len(string_split_minus)):
+    num = 0
+    s_split_plus = string_split_minus[i].split("+")
+    for c in s_split_plus:
+        num += int(c)
+    if i == 0:
+        result += num
+    else:
+        result -= num
+print(result)
