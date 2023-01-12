525296e414e719be60b186ab05e12f2121e42b12
DESKTOP-086PDKP\UltimateUser <jcnsh4ck4wllaaa@gmail.com>
2023-01-12 22:45:30 +0900

    no message

--- python/0~2499/2302-극장 좌석.py
+++ python/0~2499/2302-극장 좌석.py
@@ -25,3 +25,31 @@ if M > 0:
     print(result)
 else:
     print(dp[N])
+
+
+# ! 230112 복습
+# import sys
+
+
+# input = sys.stdin.readline
+
+# n = int(input())
+# m = int(input())
+# vip = [int(input()) for _ in range(m)]
+
+# # x개의 좌석에서 섞어서 않는 경우의 수
+# dp = [0, 1, 2, 3]
+
+# for i in range(4, 41):
+#     dp.append(dp[i - 1] + dp[i - 2])
+
+# answer = 1
+# previous_vip = 0
+# for k in vip:
+#     if k - previous_vip - 1 != 0:
+#         answer *= dp[k - previous_vip - 1]
+#     previous_vip = k
+
+# if n - previous_vip != 0:
+#     answer *= dp[n - previous_vip]
+# print(answer)
--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-import heapq
+import sys
 
-lst = [5, 4, 1, 2, 6, 3]
 
-heapq.heapify(lst)
-print(lst)  # [1, 2, 3, 4, 6, 5]
-
-# & hello
+input = sys.stdin.readline
