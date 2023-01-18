8a31e4e58c8d5f15b014680edad2a8772e274f6e
DESKTOP-086PDKP\UltimateUser <jcnsh4ck4wllaaa@gmail.com>
2023-01-18 19:29:50 +0900

    230118

--- python/0~2499/2032-신기한 소수.py
+++ python/0~2499/2032-신기한 소수.py
@@ -3,12 +3,29 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
+result = []
+
+
+def check_prime(number):
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
 
 
 def backtracking(number):
+    global N
     if len(str(number)) == N:
+        result.append(number)
         return
     else:
-        if 
-            backtracking
-            
+        for i in [1, 3, 7, 9]:
+            if check_prime(number * 10 + i):
+                backtracking(number * 10 + i)
+
+
+for i in [2, 3, 5, 7]:
+    backtracking(i)
+
+for num in result:
+    print(num)
