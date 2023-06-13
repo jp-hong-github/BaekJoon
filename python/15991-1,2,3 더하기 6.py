b0b5d99ec8cf6bae79d1e401e86d52ac09c6b521
JJPPH <ddkemf1502345@proton.me>
2023-06-13 13:19:32 +0900

    23.06.13

---
+++ python/15991-1,2,3 더하기 6.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+dp = [1 for _ in range(100001)]
+dp[1] = 1
+dp[2] = 2
+dp[3] = 2
+dp[4] = 3
+dp[5] = 3
+dp[6] = 6
+for i in range(7, 100001):
+    dp[i] = dp[i - 2] + dp[i - 4] + dp[i - 6]
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+
+    print(dp[n] % 1000000009)
