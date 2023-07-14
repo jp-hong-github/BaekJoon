d66880a0e33a04b1e11ba4b1572a1637f1d2514a
JJPPH <ddkemf1502345@proton.me>
2023-07-14 19:05:07 +0900

    23.07.14

--- python/2606-바이러스.py
+++ python/2606-바이러스.py
@@ -1,33 +1,26 @@
-from collections import deque
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
-pair = int(input())
-
+m = int(input())
 graph = [[] for _ in range(n + 1)]
-for _ in range(pair):
+for _ in range(m):
     x, y = map(int, input().split())
     graph[x].append(y)
     graph[y].append(x)
 
+from collections import deque
+
+q = deque([1])
 visited = [False for _ in range(n + 1)]
+visited[1] = True
 
+while q:
+    cur = q.popleft()
+    for next in graph[cur]:
+        if visited[next] is False:
+            visited[next] = True
+            q.append(next)
 
-def bfs(start):
-    q = deque([start])
-    result = 0
-    while q:
-        current = q.popleft()
-        for next in graph[current]:
-            if not visited[next]:
-                visited[next] = True
-                result += 1
-                q.append(next)
-    return result
-
-
-print(bfs(1) - 1)
-# print(graph)
-# print(visited)
+print(visited.count(True) - 1)
